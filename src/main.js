bd12e94c35e8dc87172597963b7ff86ec81d37c6
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-04-10 18:16:04 +0600

    update almuni, accordian, faq and footer section

--- src/main.js
+++ src/main.js
@@ -1,2 +1,31 @@
 import './style.css';
 
+document.addEventListener('DOMContentLoaded', function () {
+  function toggleAccordion(index) {
+    const content = document.getElementById(`accordion-content-${index}`);
+    const icon = document.getElementById(`icon-${index}`);
+    content.classList.toggle('show');
+    icon.classList.toggle('rotate-180');
+  }
+
+  window.toggleAccordion = toggleAccordion; // Make function accessible from inline onclick
+});
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+      const faqItem = button.closest('div');
+      const answer = faqItem.querySelector('div:last-child');
+      const svg = button.querySelector('svg');
+      const plusIcon = svg.querySelector('#plus-icon');
+      const minusIcon = svg.querySelector('#minus-icon');
+      
+      // Toggle answer visibility
+      answer.classList.toggle('hidden');
+      
+      // Toggle between plus and minus icons
+      plusIcon.classList.toggle('show');
+      minusIcon.classList.toggle('hidden');
+      
+    });
+  });
+
